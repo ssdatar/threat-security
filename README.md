90748d2e0cdc6fe5e0d372f43e000905ba8cca60
Saurabh Datar <sdatar2@stanford.edu>
2021-02-05 14:31:46 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Threat security model
+
+# Setup
+Your `node_modules` should've already been installed. Run `npm i` if you want to add some new packages.
+
+Run local server: `npm start`
+
+# Build
+
+This should build the project with all its assets in the `build` directory
+
+`npm run build`
+
+# Deploy
+
+This uses the `gh-pages` module to deploy to Github Pages of the account the repo is on. The module should already be installed when you installed all other modules.
+
+`npm run deploy`
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+module.exports = {
+	entry: './src/dev/js/entry.js',
+	output: {
+		path: path.resolve(__dirname, 'src'),
+		filename: 'main.js',
+	},
+	module: {
+    rules: [
+      {
+        test: /\.csv?$/,
+        use: ['dsv-loader']
+      },
+      {
+        test: /\.json?$/,
+        use: ['json-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['env']
+              ]
+            }
+          },
+          {
+            loader: 'eslint-loader',
+            options: {
+              failOnError: true
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
